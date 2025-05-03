d5e659c58d655ad23262e8c71e0232fdbef96459
Sazidul islam <rabbiking00@gmail.com>
2025-05-03 18:22:20 +0600

    initialized commit

---
+++ lib/features/auth/widget/dummy_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DummyWidget extends StatelessWidget {
+  const DummyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:jsulima/app.dart';
+
+void main() {
+  runApp(const Jsulima());
+}

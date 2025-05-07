6132e27bc106d811d94896a2261a28ffb4ea6ea6
shakibimtiaz <shakibimtiazjishan1111@gmail.com>
2025-05-08 02:01:49 +0600

    profile setup done

--- lib/features/profile/screen/profile_screen.dart
+++ lib/features/profile/screen/profile_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:jsulima/core/common/styles/global_text_style.dart';
+import 'package:jsulima/core/utils/constants/image_path.dart' show ImagePath;
 import 'package:jsulima/features/profile/controller/profile_info_controller.dart';
 import 'package:jsulima/features/profile/controller/notifications_controller.dart';
 import 'package:jsulima/features/profile/screen/change_password_screen.dart';
@@ -7,97 +9,102 @@ import 'package:jsulima/features/profile/screen/personal_info_screen.dart';
 import 'package:jsulima/features/profile/widgest/profile_tile_widget.dart';
 
 class ProfileScreen extends StatelessWidget {
-  final ProfileInfoController controller = Get.put(ProfileInfoController());
+  
+
+   ProfileScreen({super.key});
+
+   final ProfileInfoController controller = Get.put(ProfileInfoController());
   final NotificationsController controller2 = Get.put(
     NotificationsController(),
   );
 
   @override
   Widget build(BuildContext context) {
+ 
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Stack(
-        children: [
-          Positioned.fill(
-            child: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/images/splashImage.png'),
-                  fit: BoxFit.cover,
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(ImagePath.splashBg),
+            fit: BoxFit.cover,
+          ), 
+        ),
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: 65, 
+            left: 16, 
+            right: 16, 
+            bottom: 20, 
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+                  children: [
+                   
+                    CircleAvatar(
+                      radius: 50,
+                      backgroundImage: AssetImage(ImagePath.profileImage),
+                    ),
+                     SizedBox(height: 10),
+                    Text(
+                      controller.fullName,
+                      style:  getTextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ), 
+                    Text(
+                      controller.userNameStatic,
+                      style: getTextStyle(color: Colors.grey),
+                    ),
+                     SizedBox(height: 30),
+                    GestureDetector(
+                      onTap: () => Get.to(() => PersonalInfoScreen()),
+                      child: ProfileTileWidget(
+                        title: "Personal Information",
+                        icon: Icons.arrow_forward,
+                      ),
+                    ),
+                    Obx(
+                      () => ProfileTileWidget(
+                        title: "Notification Preferences",
+                        icon: Icons.arrow_forward,
+                        toggleValue: controller2.isNotificationEnabled.value,
+                        onToggle: controller2.toggleNotification,
+                      ),
+                    ),
+                    ProfileTileWidget(
+                      title: "Subscription",
+                      icon: Icons.arrow_forward,
+                    ),
+                    GestureDetector(
+                      onTap: () => Get.to(() => ChangePasswordScreen()),
+                      child: ProfileTileWidget(
+                        title: "Change Password",
+                        icon: Icons.arrow_forward,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ), 
+                    ProfileTileWidget(
+                      title: "Log Out",
+                      icon: Icons.power_settings_new,
+                      isLogout: true,
+                    ),
+                     SizedBox(height: 20),
+                  ],
                 ),
-              ),
+          ),
             ),
           ),
-          SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20.0,
-                vertical: 10,
-              ),
-              child: Column(
-                children: [
-                  const Align(
-                    alignment: Alignment.topLeft,
-                    child: Icon(Icons.arrow_back, color: Colors.white),
-                  ),
-                  const SizedBox(height: 20),
-                  CircleAvatar(
-                    radius: 50,
-                    backgroundImage: AssetImage('assets/images/profile.png'),
-                  ),
-                  const SizedBox(height: 10),
-                  Text(
-                    controller.fullName,
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    controller.userNameStatic,
-                    style: const TextStyle(color: Colors.grey),
-                  ),
-                  const SizedBox(height: 30),
-                  GestureDetector(
-                    onTap: () => Get.to(() => PersonalInfoScreen()),
-                    child: ProfileTileWidget(
-                      title: "Personal Information",
-                      icon: Icons.arrow_forward,
-                    ),
-                  ),
-                  Obx(
-                    () => ProfileTileWidget(
-                      title: "Notification Preferences",
-                      icon: Icons.arrow_forward,
-                      toggleValue: controller2.isNotificationEnabled.value,
-                      onToggle: controller2.toggleNotification,
-                    ),
-                  ),
-                  ProfileTileWidget(
-                    title: "Subscription",
-                    icon: Icons.arrow_forward,
-                  ),
-                  GestureDetector(
-                    onTap: () => Get.to(() => ChangePasswordScreen()),
-                    child: ProfileTileWidget(
-                      title: "Change Password",
-                      icon: Icons.arrow_forward,
-                    ),
-                  ),
-                  const Spacer(),
-                  ProfileTileWidget(
-                    title: "Log Out",
-                    icon: Icons.power_settings_new,
-                    isLogout: true,
-                  ),
-                  const SizedBox(height: 20),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
+      
+      ); 
+      
   }
 }

6710d373ae856a5d543fbf9493980e0e23ba41b6
AvijitDasAvi <avijitavi338895@gmail.com>
2025-05-07 23:37:13 +0600

    notification screen complete

--- lib/features/bottom_navbar/screen/bottom_navbar_screen.dart
+++ lib/features/bottom_navbar/screen/bottom_navbar_screen.dart
@@ -6,7 +6,7 @@ import 'package:jsulima/features/bottom_navbar/controller/bottom_navbar_controll
 import 'package:jsulima/features/games/screen/games_screen.dart';
 
 import 'package:jsulima/features/home/screen/home_screen.dart';
-
+import 'package:jsulima/features/notifications/screen/notification_screen.dart';
 
 class BottomNavbarScreen extends StatelessWidget {
   BottomNavbarScreen({super.key});
@@ -16,7 +16,7 @@ class BottomNavbarScreen extends StatelessWidget {
   final List<Widget> screens = [
     HomeScreen(),
     GamesScreen(),
-    Center(child: Text('Screen 3', style: TextStyle(color: Colors.white))),
+    NotificationScreen(),
     Center(child: Text('Screen 4', style: TextStyle(color: Colors.white))),
   ];
 

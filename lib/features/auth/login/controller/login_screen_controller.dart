35a4e862bc56183eac13d34f0ee643a0a756b168
AvijitDasAvi <avijitavi338895@gmail.com>
2025-10-01 09:39:43 +0600

    Remove sign In with google.

--- lib/features/auth/login/controller/login_screen_controller.dart
+++ lib/features/auth/login/controller/login_screen_controller.dart
@@ -1,11 +1,9 @@
 import 'dart:convert';
-import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/foundation.dart' show kDebugMode;
 import 'package:flutter/widgets.dart' show TextEditingController;
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http show post;
-import 'package:jsulima/core/services/auth_service.dart' show AuthService;
 import 'package:jsulima/core/services/end_points.dart' show Urls;
 import 'package:jsulima/core/services/shared_preferences_helper.dart'
     show SharedPreferencesHelper;
@@ -21,14 +19,14 @@ class LoginScreenController extends GetxController {
   var isRememberMeChecked = false.obs;
   var isEmailPrivate = false.obs;
 
-  final AuthService _authService = AuthService();
-  final FirebaseAnalytics _analytics = FirebaseAnalytics.instance;
+  // final AuthService _authService = AuthService();
+  // final FirebaseAnalytics _analytics = FirebaseAnalytics.instance;
 
-  @override
-  void onInit() {
-    super.onInit();
-    _analytics.setAnalyticsCollectionEnabled(false);
-  }
+  // @override
+  // void onInit() {
+  //   super.onInit();
+  //   _analytics.setAnalyticsCollectionEnabled(false);
+  // }
 
   void togglePasswordVisibility() {
     isPasswordVisible.value = !isPasswordVisible.value;
@@ -95,71 +93,71 @@ class LoginScreenController extends GetxController {
     }
   }
 
-  Future<void> signInWithGoogle() async {
-    try {
-      EasyLoading.show(status: "Signing in with Google...");
-      final userData = await _authService.signInWithGoogle();
-      if (userData != null) {
-        final data = {"email": userData['email'], "name": userData['name']};
+  // Future<void> signInWithGoogle() async {
+  //   try {
+  //     EasyLoading.show(status: "Signing in with Google...");
+  //     final userData = await _authService.signInWithGoogle();
+  //     if (userData != null) {
+  //       final data = {"email": userData['email'], "name": userData['name']};
 
-        final response = await http.post(
-          Uri.parse(Urls.googleLogin),
-          headers: {'Content-Type': 'application/json'},
-          body: jsonEncode(data),
-        );
+  //       final response = await http.post(
+  //         Uri.parse(Urls.googleLogin),
+  //         headers: {'Content-Type': 'application/json'},
+  //         body: jsonEncode(data),
+  //       );
 
-        var body = jsonDecode(response.body);
+  //       var body = jsonDecode(response.body);
 
-        if (kDebugMode) {
-          print("API Response: $body");
-        }
+  //       if (kDebugMode) {
+  //         print("API Response: $body");
+  //       }
 
-        if (response.statusCode == 201) {
-          EasyLoading.showSuccess("The user logged in successfully");
-          var accessToken = body['accessToken'] ?? body['access_token'];
-          var role = body['user']['role'];
-          var isSubscribed = body['user']['isSubscribed'];
-          var userId = body['user']['id'];
+  //       if (response.statusCode == 201) {
+  //         EasyLoading.showSuccess("The user logged in successfully");
+  //         var accessToken = body['accessToken'] ?? body['access_token'];
+  //         var role = body['user']['role'];
+  //         var isSubscribed = body['user']['isSubscribed'];
+  //         var userId = body['user']['id'];
 
-          if (accessToken is! String || role is! String || userId is! String) {
-            if (kDebugMode) {
-              print(
-                "Type mismatch: accessToken=$accessToken, role=$role, userId=$userId",
-              );
-            }
-            EasyLoading.showError("Invalid response format");
-            await _authService.signOut();
-            return;
-          }
+  //         if (accessToken is! String || role is! String || userId is! String) {
+  //           if (kDebugMode) {
+  //             print(
+  //               "Type mismatch: accessToken=$accessToken, role=$role, userId=$userId",
+  //             );
+  //           }
+  //           EasyLoading.showError("Invalid response format");
+  //           await _authService.signOut();
+  //           return;
+  //         }
 
-          await SharedPreferencesHelper.saveEmailPrivacy(false);
+  //         await SharedPreferencesHelper.saveEmailPrivacy(false);
 
-          await SharedPreferencesHelper.saveTokenAndRole(
-            accessToken,
-            role,
-            userId,
-          );
-          await SharedPreferencesHelper.isSubscribed(isSubscribed);
+  //         await SharedPreferencesHelper.saveTokenAndRole(
+  //           accessToken,
+  //           role,
+  //           userId,
+  //         );
+  //         await SharedPreferencesHelper.isSubscribed(isSubscribed);
 
-          if (isSubscribed == null || !isSubscribed) {
-            Get.offAll(() => ChooseYourPlanScreen());
-          } else {
-            Get.offAll(() => BottomNavbarScreen());
-          }
-        } else {
-          EasyLoading.showError(body['message'] ?? 'Google sign-in failed');
-          await _authService.signOut();
-        }
-      } else {
-        EasyLoading.showError('Google sign-in was cancelled');
-      }
-    } catch (e) {
-      if (kDebugMode) {
-        EasyLoading.showError("Something went wrong");
-        print("Google Sign-In Exception: $e");
-      }
-    } finally {
-      EasyLoading.dismiss();
-    }
-  }
+  //         if (isSubscribed == null || !isSubscribed) {
+  //           Get.offAll(() => ChooseYourPlanScreen());
+  //         } else {
+  //           Get.offAll(() => BottomNavbarScreen());
+  //         }
+  //       } else {
+  //         EasyLoading.showError(body['message'] ?? 'Google sign-in failed');
+  //         await _authService.signOut();
+  //       }
+  //     } else {
+  //       EasyLoading.showError('Google sign-in was cancelled');
+  //     }
+  //   } catch (e) {
+  //     if (kDebugMode) {
+  //       EasyLoading.showError("Something went wrong");
+  //       print("Google Sign-In Exception: $e");
+  //     }
+  //   } finally {
+  //     EasyLoading.dismiss();
+  //   }
+  // }
 }
